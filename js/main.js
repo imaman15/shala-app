8aacce58f9cd5f1388799971ba7db8e40d93ce2c
imaman15 <imamagustiannugraha@ymail.com>
2021-04-20 16:23:15 +0700

    Features Section Done

--- js/main.js
+++ js/main.js
@@ -25,6 +25,7 @@ $(document).ready(function () {
   $('.features-carousel').owlCarousel({
     loop: true,
     margin: 0,
+    autoplay: true,
     responsiveClass: true,
     responsive: {
       0: {
